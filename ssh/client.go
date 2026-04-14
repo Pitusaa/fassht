71b3faac87734ffc63826b6f096b35f1099e1b4c
Pitusa <juanbautip12@gmail.com>
2026-04-14 02:58:15 +0200

    fix: try default SSH keys (~/.ssh/id_ed25519 etc) when no IdentityFile set

--- ssh/client.go
+++ ssh/client.go
@@ -38,14 +38,20 @@ func BuildAuthMethods(host config.SSHHost) ([]gossh.AuthMethod, net.Conn) {
 		}
 	}
 
-	// Try identity file
+	// Try identity file from SSH config
 	if host.IdentityFile != "" {
-		keyPath := expandTilde(host.IdentityFile)
-		key, err := os.ReadFile(keyPath)
-		if err == nil {
-			signer, err := gossh.ParsePrivateKey(key)
-			if err == nil {
+		if signer := loadKeySigner(expandTilde(host.IdentityFile)); signer != nil {
+			methods = append(methods, gossh.PublicKeys(signer))
+		}
+	}
+
+	// Fall back to default key locations (mirrors OpenSSH behaviour)
+	if host.IdentityFile == "" {
+		home, _ := os.UserHomeDir()
+		for _, name := range []string{"id_ed25519", "id_ecdsa", "id_rsa"} {
+			if signer := loadKeySigner(filepath.Join(home, ".ssh", name)); signer != nil {
 				methods = append(methods, gossh.PublicKeys(signer))
+				break
 			}
 		}
 	}
@@ -115,6 +121,19 @@ func (c *Client) Close() error {
 		}
 	}
 	return firstErr
+}
+
+// loadKeySigner reads a private key file and returns a signer, or nil if it fails.
+func loadKeySigner(path string) gossh.Signer {
+	key, err := os.ReadFile(path)
+	if err != nil {
+		return nil
+	}
+	signer, err := gossh.ParsePrivateKey(key)
+	if err != nil {
+		return nil
+	}
+	return signer
 }
 
 func expandTilde(path string) string {
